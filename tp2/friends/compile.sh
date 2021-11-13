95d7b772cffba087a34c5a3b967e6f21d30b5baf
ALI Akbar SABZI <57127882+sabzi1984@users.noreply.github.com>
2021-11-12 19:17:32 -0500

    Delete tp2/friends directory

--- tp2/friends/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-${JAVA_HOME}/bin/javac -classpath `${HADOOP_HOME}/bin/hadoop classpath` -d classes Friends.java
-${JAVA_HOME}/bin/jar -cvf friends.jar -C classes/ .
-
-rm -rf classes/*
